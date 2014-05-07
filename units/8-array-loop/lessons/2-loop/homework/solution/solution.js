ceec5ae85e3231c57385dd003ccd722e4263e5a4
Manish Sinha <manish@qhojo.com>
2014-05-07 14:07:51 -0400

    Create solution.js

---
+++ units/8-array-loop/lessons/2-loop/homework/solution/solution.js
@@ -0,0 +1,99 @@
+// 1
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+console.log("Ariana Grande");
+
+
+// 2
+var i=0;
+
+while (i < 25)
+{
+  console.log("Ariana Grande");
+  i=i+1;
+}
+
+// 3
+var i=0;
+
+while (i < 15)
+{
+  console.log(i);
+  i=i+1;
+}
+
+// 4
+var i=2;
+
+while (i < 17)
+{
+  console.log(i);
+  i=i+1;
+}
+
+// 5
+for (var i=0; i<25; i=i+1)
+{
+  console.log("Ariana Grande");
+}
+
+// 6
+for (var i=0; i<15; i=i+1)
+{
+  console.log(i);
+}
+
+// 7
+for (var i=2; i<17; i=i+1)
+{
+  console.log(i);
+}
+
+// 8
+for (var i=0; i<15; i=i+1)
+{
+  if (i==7)
+    break;
+  
+  console.log(i);
+}
+
+// 9
+for (var i=2; i<17; i=i+1)
+{
+  if (i==6)
+    continue;
+    
+  console.log(i);
+}
+
+// 10
+var animals = ["tiger", "lion", "bobcat", "bear", "fish"];
+
+// 11
+for (var i=0; i<animals.length; i=i+1)
+{
+  console.log(animals[i]);
+}
