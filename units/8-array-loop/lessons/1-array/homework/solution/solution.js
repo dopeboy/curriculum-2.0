75dc2aae0ff221a4e70b095fc69e37f19db5b59b
Manish Sinha <manish@qhojo.com>
2014-05-07 13:25:10 -0400

    Update solution.js

--- units/8-array-loop/lessons/1-array/homework/solution/solution.js
+++ units/8-array-loop/lessons/1-array/homework/solution/solution.js
@@ -1,20 +1,52 @@
-// 1
-var groceries = ["banana", "pepper", "lime"];
-
 // 2
-console.log(groceries.length);
+var groceries = ["cereal", "bread", "eggs"];
 
 // 3
-console.log(groceries[0]);
-console.log(groceries[2]) OR console.log(groceries[groceries.length-1]);
+console.log(groceries.length);
 
 // 4
-groceries.push("ice-cream");
-groceries.push("salt");
-groceries.push("chicken");
+console.log(groceries.join());
 
 // 5
-console.log(groceries[0]);
-console.log(groceries[5]) OR console.log(groceries[groceries.length-1]);
+groceries.unshift("chicken");
+groceries.unshift("salt");
+groceries.unshift("ice-cream");
+
+
+// 6
+groceries.push("beef");
+groceries.push("rice");
+groceries.push("sugar");
+
+// 7
+console.log(groceries.length);
+
+// 8
+console.log(groceries.join());
+
+// 9
+console.log(groceries.shift());
+console.log(groceries.pop());
+
+// 10
+console.log(groceries.length);
+
+// 11
+console.log(groceries.join());
+
+// 12
+console.log(groceries.slice(0,2).join());
+
+// 13
+var parents_groceries = ["flour", "cream"];
+var final_groceries = groceries.concat(parents_groceries);
+
+// 14
+console.log(final_groceries.length);
+
+// 15
+console.log(final_groceries.join());
+
+
 
 
