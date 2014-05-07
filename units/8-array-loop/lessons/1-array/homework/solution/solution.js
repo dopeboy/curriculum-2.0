4f487c6769a752bc765215ecd1fbd329e8ce4500
Manish Sinha <manish@qhojo.com>
2014-05-07 15:18:27 -0400

    Update solution.js

--- units/8-array-loop/lessons/1-array/homework/solution/solution.js
+++ units/8-array-loop/lessons/1-array/homework/solution/solution.js
@@ -1,52 +1,106 @@
-// 2
+console.log("**********START OF EXERCISE 2**********");
+
 var groceries = ["cereal", "bread", "eggs"];
 
-// 3
+console.log("**********END OF EXERCISE 2**********");
+
+
+console.log("**********START OF EXERCISE 3**********");
+
 console.log(groceries.length);
 
-// 4
+console.log("**********END OF EXERCISE 3**********");
+
+
+console.log("**********START OF EXERCISE 4**********");
+
 console.log(groceries.join());
 
-// 5
+console.log("**********END OF EXERCISE 4**********");
+
+
+console.log("**********START OF EXERCISE 5**********");
+
 groceries.unshift("chicken");
 groceries.unshift("salt");
 groceries.unshift("ice-cream");
 
+console.log("**********END OF EXERCISE 5**********");
 
-// 6
+
+console.log("**********START OF EXERCISE 6**********");
+
 groceries.push("beef");
 groceries.push("rice");
 groceries.push("sugar");
 
-// 7
+console.log("**********END OF EXERCISE 6**********");
+
+
+console.log("**********START OF EXERCISE 7**********");
+
 console.log(groceries.length);
 
-// 8
+console.log("**********END OF EXERCISE 7**********");
+
+
+console.log("**********START OF EXERCISE 8**********");
+
 console.log(groceries.join());
 
-// 9
+
+console.log("**********END OF EXERCISE 8**********");
+
+
+console.log("**********START OF EXERCISE 9**********");
+
 console.log(groceries.shift());
 console.log(groceries.pop());
 
-// 10
+console.log("**********END OF EXERCISE 9**********");
+
+
+console.log("**********START OF EXERCISE 10**********");
+
 console.log(groceries.length);
 
-// 11
+console.log("**********END OF EXERCISE 10**********");
+
+
+console.log("**********START OF EXERCISE 11**********");
+
 console.log(groceries.join());
 
-// 12
+console.log("**********END OF EXERCISE 11**********");
+
+
+console.log("**********START OF EXERCISE 12**********");
+
 console.log(groceries.slice(0,2).join());
 
-// 13
+console.log("**********END OF EXERCISE 12**********");
+
+
+console.log("**********START OF EXERCISE 13**********");
+
 var parents_groceries = ["flour", "cream"];
 var final_groceries = groceries.concat(parents_groceries);
 
-// 14
+console.log("**********END OF EXERCISE 13**********");
+
+
+console.log("**********START OF EXERCISE 14**********");
+
 console.log(final_groceries.length);
 
-// 15
+console.log("**********END OF EXERCISE 14**********");
+
+
+console.log("**********START OF EXERCISE 15**********");
+
 console.log(final_groceries.join());
 
+console.log("**********END OF EXERCISE 15**********");
 
 
 
