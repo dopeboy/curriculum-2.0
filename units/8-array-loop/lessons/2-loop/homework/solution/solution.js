7ae1a3ba37065cd867c731abbc12d3444796e9db
Manish Sinha <manish@qhojo.com>
2014-05-07 14:17:42 -0400

    Update solution.js

--- units/8-array-loop/lessons/2-loop/homework/solution/solution.js
+++ units/8-array-loop/lessons/2-loop/homework/solution/solution.js
@@ -1,4 +1,5 @@
-// 1
+console.log("**********START OF EXERCISE 1**********");
+
 console.log("Ariana Grande");
 console.log("Ariana Grande");
 console.log("Ariana Grande");
@@ -25,8 +26,11 @@ console.log("Ariana Grande");
 console.log("Ariana Grande");
 console.log("Ariana Grande");
 
+console.log("**********END OF EXERCISE 1**********");
 
-// 2
+
+console.log("**********START OF EXERCISE 2**********");
+
 var i=0;
 
 while (i < 25)
@@ -35,7 +39,11 @@ while (i < 25)
   i=i+1;
 }
 
-// 3
+console.log("**********END OF EXERCISE 2**********");
+
+
+console.log("**********START OF EXERCISE 3**********");
+
 var i=0;
 
 while (i < 15)
@@ -44,7 +52,11 @@ while (i < 15)
   i=i+1;
 }
 
-// 4
+console.log("**********END OF EXERCISE 3**********");
+
+
+console.log("**********START OF EXERCISE 4**********");
+
 var i=2;
 
 while (i < 17)
@@ -53,25 +65,41 @@ while (i < 17)
   i=i+1;
 }
 
-// 5
+console.log("**********END OF EXERCISE 4**********");
+
+
+console.log("**********START OF EXERCISE 5**********");
+
 for (var i=0; i<25; i=i+1)
 {
   console.log("Ariana Grande");
 }
 
-// 6
+console.log("**********END OF EXERCISE 5**********");
+
+
+console.log("**********START OF EXERCISE 6**********");
+
 for (var i=0; i<15; i=i+1)
 {
   console.log(i);
 }
 
-// 7
+console.log("**********END OF EXERCISE 6**********");
+
+
+console.log("**********START OF EXERCISE 7**********");
+
 for (var i=2; i<17; i=i+1)
 {
   console.log(i);
 }
 
-// 8
+console.log("**********END OF EXERCISE 7**********");
+
+
+console.log("**********START OF EXERCISE 8**********");
+
 for (var i=0; i<15; i=i+1)
 {
   if (i==6)
@@ -80,7 +108,11 @@ for (var i=0; i<15; i=i+1)
   console.log(i);
 }
 
-// 9
+console.log("**********END OF EXERCISE 8**********");
+
+
+console.log("**********START OF EXERCISE 9**********");
+
 for (var i=2; i<17; i=i+1)
 {
   if (i==6)
@@ -89,11 +121,21 @@ for (var i=2; i<17; i=i+1)
   console.log(i);
 }
 
-// 10
+console.log("**********END OF EXERCISE 9**********");
+
+
+console.log("**********START OF EXERCISE 10**********");
+
 var animals = ["tiger", "lion", "bobcat", "bear", "fish"];
 
-// 11
+console.log("**********END OF EXERCISE 10**********");
+
+
+console.log("**********START OF EXERCISE 11**********");
+
 for (var i=0; i<animals.length; i=i+1)
 {
   console.log(animals[i]);
 }
+
+console.log("**********END OF EXERCISE 11**********");
