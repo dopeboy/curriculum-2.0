6e770a87d6f3c09c1c3855e0fd4ad8d39b385bc7
Manish Sinha <manish@qhojo.com>
2014-05-08 19:23:02 -0400

    Create solution.js

---
+++ units/7-conditional/lessons/1-conditional/homework/solution/solution.js
@@ -0,0 +1,97 @@
+console.log("**********START OF EXERCISE 1**********");
+
+var bieber = false;
+var timberlake = false;
+
+console.log("**********END OF EXERCISE 1**********");
+
+
+console.log("**********START OF EXERCISE 2**********");
+
+console.log(bieber);
+console.log(timberlake);
+
+console.log("**********END OF EXERCISE 2**********");
+
+
+console.log("**********START OF EXERCISE 3**********");
+
+bieber = true;
+
+console.log("**********END OF EXERCISE 3**********");
+
+
+console.log("**********START OF EXERCISE 4**********");
+
+if (bieber == true)
+{
+  console.log("I am Justin Bieber. I'll never let you go.");
+}
+
+console.log("**********END OF EXERCISE 4**********");
+
+
+console.log("**********START OF EXERCISE 5**********");
+
+if (timberlake == false)
+{
+  console.log("I am not Justin Timberlake. It's not like you're my mirror.");
+}
+
+console.log("**********END OF EXERCISE 5**********");
+
+
+console.log("**********START OF EXERCISE 6**********");
+
+if (bieber == true || timberlake == true)
+{
+  console.log("My first name is Justin.");
+}
+
+console.log("**********END OF EXERCISE 6**********");
+
+
+console.log("**********START OF EXERCISE 7**********");
+
+timberlake = true;
+
+console.log("**********END OF EXERCISE 7**********");
+
+
+console.log("**********START OF EXERCISE 8**********");
+
+if (bieber == true && timberlake == true)
+{
+  console.log("I am a rock star.");
+}
+
+console.log("**********END OF EXERCISE 8**********");
+
+
+console.log("**********START OF EXERCISE 9**********");
+
+// Note, more than one right answer here.
+
+var color = prompt("What color are you?");
+
+if (color == "Yellow")
+{
+  var sea = prompt("Do you live in the sea?");
+  
+  if (sea == "Yes")
+  {
+    console.log("You are Spongebob Squarepants!");
+  }
+  
+  else
+  {
+    console.log("You are Pikachu!");
+  }
+}
+
+else
+{
+  console.log("You are Mario!");
+}
+
+console.log("**********END OF EXERCISE 9**********");
