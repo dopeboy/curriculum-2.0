6fbc43150d7ee51402b037121e23166c84ca787f
Manish Sinha <manish@qhojo.com>
2014-06-12 19:35:48 -0400

    Create solution.js

---
+++ units/4-javascript/1-variable/homework/solution/solution.js
@@ -0,0 +1,57 @@
+console.log("**********START OF EXERCISE 1**********");
+
+var degreesInCelsius = 35;
+
+console.log("**********END OF EXERCISE 1**********");
+
+
+console.log("**********START OF EXERCISE 2**********");
+
+var degreesInFahrenheight = ((degreesInCelsius*9)/5)+32;
+
+console.log("**********END OF EXERCISE 2**********");
+
+
+console.log("**********START OF EXERCISE 3**********");
+
+console.log(degreesInFahrenheight);
+
+console.log("**********END OF EXERCISE 3**********");
+
+
+console.log("**********START OF EXERCISE 4**********");
+
+var degreesInCelsius = (degreesInFahrenheight-32) * (5/9);
+
+console.log("**********END OF EXERCISE 4**********");
+
+
+console.log("**********START OF EXERCISE 5**********");
+
+console.log(degreesInCelsius);
+
+console.log("**********END OF EXERCISE 5**********");
+
+
+console.log("**********START OF EXERCISE 6**********");
+
+var degreesInKelvin = degreesInCelsius + 273;
+
+console.log("**********END OF EXERCISE 6**********");
+
+
+console.log("**********START OF EXERCISE 7**********");
+
+console.log(degreesInKelvin);
+
+console.log("**********END OF EXERCISE 7**********");
+
+
+console.log("**********START OF EXERCISE 8**********");
+
+console.log(typeof(degreesInCelsius));
+console.log(typeof(degreesInFahrenheight));
+console.log(typeof(degreesInKelvin));
+
+console.log("**********END OF EXERCISE 8**********");
+
