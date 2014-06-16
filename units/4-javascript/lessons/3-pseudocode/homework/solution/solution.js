2dca54d99523e0b9c97af5d77e4de172762d2b3f
Manish Sinha <manish@qhojo.com>
2014-06-16 19:21:15 -0400

    Create solution.js

---
+++ units/4-javascript/lessons/3-pseudocode/homework/solution/solution.js
@@ -0,0 +1,18 @@
+console.log("**********START OF EXERCISE 4**********");
+
+function solveQuadraticEquation(a,b,c)
+{
+  var inside = Math.sqrt(Math.pow(b,2) - (4*a*c));
+  var numeratorOne = (-1)*b + inside;
+  var numeratorTwo = (-1)*b - inside;
+  
+  var answerOne = numeratorOne / (2*a);
+  var answerTwo = numeratorTne / (2*a);
+  
+  console.log(answerOne);
+  console.log(answerTwo);
+}
+
+console.log("**********END OF EXERCISE 4**********");
+
+
