59da5fbdc3728af499e117ea03bafdc381762fbc
Manish Sinha <manish@qhojo.com>
2014-08-07 22:38:58 -0400

    Update wall.js

--- units/9-jquery/lessons/1-jquery/homework/solution/wall.js
+++ units/9-jquery/lessons/1-jquery/homework/solution/wall.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('#button').click(function(){
+    $('button').click(function(){
         var post = $('#postText').val();
         
         $('#wall').prepend("<div id='like'>" + post + "<br><div class='like'>Like</div><br><br></div>");
