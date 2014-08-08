63b4d35baf73becb09b134c9567dd9ab4355150c
Manish Sinha <manish@qhojo.com>
2014-08-07 22:37:24 -0400

    Create wall.js

---
+++ units/9-jquery/lessons/1-jquery/homework/solution/wall.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('#button').click(function(){
+        var post = $('#postText').val();
+        
+        $('#wall').prepend("<div id='like'>" + post + "<br><div class='like'>Like</div><br><br></div>");
+            
+        $('.like').click(function(){
+            $(this).text("Unlike");
+            $(this).toggleClass("unlike");
+            
+        $('.unlike').click(function(){
+            $(this).text("Like");
+            $(this).toggleClass("like");
+
+        });
+        });
+    });
+});
+
