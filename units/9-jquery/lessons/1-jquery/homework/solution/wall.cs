71c3199f3a09d9e94874ea9ca5c95dfb1e72f6d3
Manish Sinha <manish@qhojo.com>
2014-08-07 22:38:50 -0400

    Create wall.cs

---
+++ units/9-jquery/lessons/1-jquery/homework/solution/wall.cs
@@ -0,0 +1,46 @@
+#wall
+{
+  border: 1px solid #CEC3B4;
+  height:400px;
+  width: 550px;
+  overflow: auto;
+  position: absolute; 
+  right: 20.4%; 
+  top: 50%;
+}
+
+#postText
+{
+    border: 1px solid #CEC3B4;
+    position:absolute;
+    left: 38.7%;
+    top: 39%;
+    width: 550px;
+    height: 60px;
+}
+
+button
+{
+    position: absolute; 
+    left: 74%;
+    top: 43.5%;
+    width: 70px;
+    height: 30px;
+    background-color: #425AA0;
+    color: white;
+    font-weight: bold;
+    border: 1px solid #3333AD;
+    border-radius: 4px;
+}
+
+.like
+{
+   color: #425AA0; 
+   font-weight: bold;
+}
+
+.comment
+{
+    color: #425AA0; 
+    font-weight: bold;
+}
